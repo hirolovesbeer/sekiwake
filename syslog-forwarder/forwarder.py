04ebe608443589b48d44b9ccc6c0fdbab7c35422
Hiroshi <hirolovesbeer@gmail.com>
2017-01-26 17:56:22 +0900

    use recvfrom -> recv

--- syslog-forwarder/forwarder.py
+++ syslog-forwarder/forwarder.py
@@ -1,6 +1,7 @@
 from socket import *
 
 BUFSIZE = 65535
+# BUFSIZE = 4096
 DST_HOST = "172.16.0.12"
 HOST = "0.0.0.0"
 PORT = 514
@@ -11,15 +12,17 @@ class SyslogForwarder():
         self._host = host
         self._port = port
         self._count = 0
+        self._sock = socket(AF_INET, SOCK_DGRAM)
 
-    def _forward(self, data):
-        if DEBUG:
-            print("Forwarding: '%s'" % (data))
-
-        sock = socket(AF_INET, SOCK_DGRAM)
-        sock.sendto(data, (DST_HOST, PORT))
-
-        self._count = self._count + 1
+#    def _forward(self, data):
+#        if DEBUG:
+#            print("Forwarding: '%s'" % (data))
+#
+#        #sock = socket(AF_INET, SOCK_DGRAM)
+#        #sock.sendto(data, (DST_HOST, PORT))
+#        self._sock.sendto(data, (DST_HOST, PORT))
+#
+#        # self._count = self._count + 1
 
     def listen(self):
         listensocket = socket(AF_INET, SOCK_DGRAM)
@@ -27,8 +30,15 @@ class SyslogForwarder():
 
         while True:
             # addr = [ipaddr, port]
-            data, addr = listensocket.recvfrom(BUFSIZE)
-            self._forward(data) # data and port
+            # data, addr = listensocket.recvfrom(BUFSIZE)
+#            data = listensocket.recv(BUFSIZE)
+#            self._count = self._count + 1
+#            # self._forward(data) # data and port
+#            self._sock.sendto(data, (DST_HOST, PORT))
+
+            # data = listensocket.recv(BUFSIZE)
+            self._count = self._count + 1
+            self._sock.sendto(listensocket.recv(BUFSIZE), (DST_HOST, PORT))
 
 if __name__ == "__main__":
     try:
