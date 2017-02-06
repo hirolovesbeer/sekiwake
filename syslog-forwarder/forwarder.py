ca21c8abbfd533dae5b3994e83bc2f2d1a00b4f0
abe <abe@impala01.clip.iiji.jp>
2017-02-06 15:10:17 +0900

    test 2 hosts forwarding

--- syslog-forwarder/forwarder.py
+++ syslog-forwarder/forwarder.py
@@ -1,11 +1,14 @@
 from socket import *
 
 BUFSIZE = 65535
-# BUFSIZE = 4096
-DST_HOST = "172.16.0.12"
+#BUFSIZE = 1024
+DST_HOST = "10.6.6.22"
+#DST_HOST2 = "10.6.6.23"
 HOST = "0.0.0.0"
 PORT = 514
 DEBUG = 0
+
+LOG_PATH = '/var/tmp/test.log'
 
 class SyslogForwarder():
     def __init__(self, host, port):
@@ -13,32 +16,26 @@ class SyslogForwarder():
         self._port = port
         self._count = 0
         self._sock = socket(AF_INET, SOCK_DGRAM)
+        self._logpath = LOG_PATH
 
-#    def _forward(self, data):
-#        if DEBUG:
-#            print("Forwarding: '%s'" % (data))
-#
-#        #sock = socket(AF_INET, SOCK_DGRAM)
-#        #sock.sendto(data, (DST_HOST, PORT))
-#        self._sock.sendto(data, (DST_HOST, PORT))
-#
-#        # self._count = self._count + 1
+    def store_log(self, msg):
+        with open(self._logpath, mode='a', encoding='utf-8') as fh:
+            fh.write(msg.decode('utf-8'))
 
     def listen(self):
         listensocket = socket(AF_INET, SOCK_DGRAM)
         listensocket.bind((self._host, self._port))
 
         while True:
-            # addr = [ipaddr, port]
-            # data, addr = listensocket.recvfrom(BUFSIZE)
-#            data = listensocket.recv(BUFSIZE)
-#            self._count = self._count + 1
-#            # self._forward(data) # data and port
-#            self._sock.sendto(data, (DST_HOST, PORT))
+            # comment: addr = [ipaddr, port]
+            data, addr = listensocket.recvfrom(BUFSIZE)
+            self._sock.sendto(data, (DST_HOST, PORT))
+#            self._sock.sendto(data, (DST_HOST2, PORT))
 
-            # data = listensocket.recv(BUFSIZE)
             self._count = self._count + 1
-            self._sock.sendto(listensocket.recv(BUFSIZE), (DST_HOST, PORT))
+#            self.store_log(data)
+#            self._sock.sendto(listensocket.recvfrom(BUFSIZE)[0], (DST_HOST, PORT))
+
 
 if __name__ == "__main__":
     try:
