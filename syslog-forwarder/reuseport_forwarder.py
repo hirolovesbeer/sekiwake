57dd9d3a0974969afce8a9b9e697a841d5bc31ed
Hiroshi ABE <hirolovesbeer@gmail.com>
2017-02-08 13:34:50 +0900

    add KeyboardInterrupt handling

--- syslog-forwarder/reuseport_forwarder.py
+++ syslog-forwarder/reuseport_forwarder.py
@@ -13,19 +13,25 @@ SO_REUSEPORT = 15
 BUFSIZE = 1024
 DST_HOST = "192.168.11.13"
 
+CONFIG = 'config.yml'
+
 def listener_work(num, dst_hosts, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, SO_REUSEPORT, 1)   # set SO_REUSEPORT
     s.bind(("", port))
 
-    while True:
-        data, addr = s.recvfrom(BUFSIZE)
-        # s.sendto(data, (DST_HOST, PORT))
-        for dst in dst_hosts:
-            s.sendto(data, (dst, port))
+    try:
+        while True:
+            data, addr = s.recvfrom(BUFSIZE)
+            # s.sendto(data, (DST_HOST, PORT))
+            for dst in dst_hosts:
+               s.sendto(data, (dst, port))
+
+    except KeyboardInterrupt:
+        print ("Crtl+C Pressed. Shutting down.")
 
 def server():
-    with open("forward.conf") as f:
+    with open(CONFIG) as f:
         config = yaml.load(f)
 
     dst_hosts = config['syslog']
@@ -41,8 +47,12 @@ def server():
         p.join()
 
 def main():
-    if '-s' in sys.argv:
+    try:
         server()
+    except(IOError, SystemExit):
+        raise
+    except KeyboardInterrupt:
+        print ("Crtl+C Pressed. Shutting down.")
 
 if __name__ == '__main__':
     main()
